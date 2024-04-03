f9ea021e5d3b1cee3d44174a792a93ae713994f0
Afonso deSousa <af.nunosousa@gmail.com>
2024-04-03 19:23:17 +0100

    Delete pascal_triangle.ml

--- pascal_triangle.ml
+++
@@ -1,8 +0,0 @@
-let rec n_k_element n k =
-  if n = k || n = 0 || k = 0 then 1
-  else n_k_element (n-1)(k-1) + n_k_element (n-1)k ;;
-
-let int k = 0;;
-
-let rec build_triangle n =
-  print_int (n_k_element n k) 
